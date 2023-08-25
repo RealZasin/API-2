4dc2735434b540a42cb2a670d25471b3be734fd4
RealZasin <113669153+RealZasin@users.noreply.github.com>
2023-08-25 18:36:43 +0200

    Muligens final touches med css design og finpussing av kode og oppsett.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
-// Select the button and image container elements from the DOM
+
 const loadCatBtn = document.getElementById("loadCatBtn");
 const catImageContainer = document.getElementById("catImageContainer");
+const catBreedDropdown = document.getElementById("catBreedDropdown");
+let catBreedIdMapping = {};
+
 
 const loadDogBtn = document.getElementById("loadDogBtn");
 const dogImageContainer = document.getElementById("dogImageContainer");
+const breedDropdown = document.getElementById("breedDropdown");
 
 const loadSuggestion = document.getElementById("funBtn");
 const boredSuggestionText = document.getElementById("boredSuggestionText");
@@ -11,20 +15,19 @@ const dropdown = document.getElementById("dropdown");
 
 
 
+// GET CATS IN --------------------------------------------------------------------------
 
-
-function loadCatImage() {
-  // Use the fetch() method to get a random cat image from the "The Cat API".
-  fetch("https://api.thecatapi.com/v1/images/search")
-    .then((response) => response.json()) // Convert the response to JSON format
+function loadCatImage(breedId = "") {
+  let apiUrl = breedId
+    ? `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+    : "https://api.thecatapi.com/v1/images/search";
+    
+  fetch(apiUrl)
+    .then((response) => response.json())
     .then((data) => {
-      const catImageUrl = data[0].url; // Extract the image URL from the returned data
-
-      // Create an <img> element and set its 'src' attribute to the fetched image URL
+      const catImageUrl = data[0].url; 
       const catImg = document.createElement("img");
       catImg.src = catImageUrl;
-
-      // Clear any previous images and append the new image to the container
       catImageContainer.innerHTML = "";
       catImageContainer.appendChild(catImg);
     })
@@ -33,23 +36,53 @@ function loadCatImage() {
     });
 }
 
-// Add a click event listener to the button
-loadCatBtn.addEventListener("click", loadCatImage);
+
+function populateCatBreedDropdown() {
+  fetch("https://api.thecatapi.com/v1/breeds")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((breed) => {
+        const option = document.createElement("option");
+        option.value = breed.id;  // using breed id as value
+        option.text = breed.name; // breed name as display text
+        catBreedDropdown.appendChild(option);
+        
+        catBreedIdMapping[breed.name] = breed.id; // Store the ID mapping
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching cat breeds:", error);
+    });
+}
+
+populateCatBreedDropdown();
+
+loadCatBtn.addEventListener("click", () => {
+  const selectedCatBreedId = catBreedDropdown.value; 
+  loadCatImage(selectedCatBreedId);
+});
+
 
 loadCatImage();
 
-function loadDogImage() {
-  fetch("https://dog.ceo/api/breeds/image/random")
+
+
+
+
+
+//GET DOGS IN -----------------------------------------------------------------------------
+
+function loadDogImage(breed = "") {
+  let apiUrl = breed
+    ? `https://dog.ceo/api/breed/${breed}/images/random`
+    : "https://dog.ceo/api/breeds/image/random";
+  
+  fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
-      // Note: The structure of the returned data from the dog API is different from the cat API.
-      // You get the image URL directly from the 'message' property.
       const dogImageUrl = data.message;
-
       const dogImg = document.createElement("img");
       dogImg.src = dogImageUrl;
-
-      // Clear any previous images and append the new image to the container.
       dogImageContainer.innerHTML = "";
       dogImageContainer.appendChild(dogImg);
     })
@@ -59,11 +92,45 @@ function loadDogImage() {
 }
 
 
-// Add a click event listener to the button
-loadDogBtn.addEventListener("click", loadDogImage);
+
+
+function populateBreedDropdown() {
+  fetch("https://dog.ceo/api/breeds/list/all")
+    .then((response) => response.json())
+    .then((data) => {
+      const breeds = Object.keys(data.message); 
+
+      
+      breeds.forEach((breed) => {
+        const option = document.createElement("option");
+        option.value = breed;
+        option.text = breed.charAt(0).toUpperCase() + breed.slice(1); 
+        breedDropdown.appendChild(option);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching dog breeds:", error);
+    });
+}
+
+populateBreedDropdown();
+
+loadDogBtn.addEventListener("click", () => {
+  const selectedBreed = breedDropdown.value;
+  loadDogImage(selectedBreed);
+});
+
 
 loadDogImage()
 
+
+
+
+
+
+
+
+//BORED TO UNBORED SECTION ----------------------------------------------------------------
 
 const types = [
 	"education",
